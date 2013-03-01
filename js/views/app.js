37048234cb6a62ad8b6e89456290d2136c887fd6
Umapathy Manohar <umapathymanohar@gmail.com>
2013-03-01 18:40:21 +0530

    Creating Repo

---
+++ js/views/app.js
@@ -0,0 +1,31 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'vm',
+	'events',
+  'text!templates/layout.html' 
+], function($, _, Backbone, Vm, Events, layoutTemplate){
+  var AppView = Backbone.View.extend({
+    el: '#video-container',
+    initialize: function () {
+      
+    },
+    render: function () {
+			var that = this;
+
+      $(this.el).html(layoutTemplate);
+      Backbone.history.start();
+		},
+       events : {
+        'click .reverse' : 'reverse',
+        // 'scroll #video-container': 'checkScroll'
+    },
+        reverse : function() {
+        var list = $('ul');
+        var listItems = list.children('li');
+        list.html(listItems.get().reverse());
+    },
+	});
+  return AppView;
+});
---
+++ js/views/youtube/widget.js
@@ -0,0 +1,50 @@
+define([
+  'jquery',
+  'underscore',
+  'backbone',
+  'vm',
+  'collections/youtube',
+  'text!templates/youtube/list.html'
+], function($, _, Backbone, Vm, YoutubeCollection, YoutubeListTemplate){
+  var YoutubeWidget = Backbone.View.extend({
+    el: '.youtube-widget',
+    initialize: function () {
+      // isLoading is a useful flag to make sure we don't send off more than
+      // one request at a time
+      this.isLoading = false;
+
+      this.youtubeCollection = new YoutubeCollection();
+      that = this;
+         $(window).scroll(function() {
+            that.checkScroll(); //change "this" to "that"
+        });
+
+    },
+    render: function () {
+      this.loadResults();
+    },
+    loadResults: function () {
+      var that = this;
+      // we are starting a new load of results so set isLoading to true
+      this.isLoading = true;
+      // fetch is Backbone.js native function for calling and parsing the collection url
+      this.youtubeCollection.fetch({ 
+        success: function (feeds) {
+          
+         $(that.el).append(_.template(YoutubeListTemplate, {feeds: feeds.toJSON()}));
+          // Now we have finished loading set isLoading back to false
+          that.isLoading = false;
+        }
+      });      
+    },
+    
+    checkScroll: function () {
+      
+      if  ($(window).scrollTop() == $(document).height() - $(window).height()){
+          this.youtubeCollection.page += 12; // Load next page
+          this.loadResults();
+        }
+    }
+  });
+  return YoutubeWidget;
+});
